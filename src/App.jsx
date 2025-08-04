c8660588828d71cc2c280104176ea2aa4ec8a75f
AhmedOg <ahmedogbinto@gmail.com>
2025-08-04 18:02:46 -0400

    Mise en place de la boxy generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
+    // Mise en place de notre application
+    <div className="min-h-screen flex flex-col">
+      <Header/>
+
+      <main className="flex flex-wrap justify-center items-center flex-grow bg-gray-100 px-10 md:flex-nowrap">
+
+      </main>
+      {/* <LeftSide/> */}
+      {/* <Visualization/> */}
+
+      <Footer/>
 
     </div>
   );
